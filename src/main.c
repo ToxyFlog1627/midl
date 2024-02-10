22922cf8111cecb6382514f4e702afba093e62e0
ToxyFlog1627 <toxypochta@gmail.com>
2024-02-10 09:39:19 +0200

    more ELF structs

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #define SYS_WRITE 1
 #define SYS_OPEN 2
 #define SYS_CLOSE 3
+#define SYS_LSEEK 8
 #define SYS_EXIT 60
 
 #define STDIN 0
@@ -11,6 +12,10 @@
 #define O_RDONLY 0
 #define O_WRONLY 1
 #define O_RDWR 2
+
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
 
 typedef long word;
 typedef unsigned long size_t;
@@ -53,13 +58,86 @@ typedef struct {
     uint16_t type;
     uint16_t arch;
     uint32_t version;
+    uint64_t entry;  // TODO: void* or main*
+    uint64_t segments_offset;
+    uint64_t sections_offset;
+    uint32_t flags;
+    uint16_t size;
+    uint16_t segment_entry_size;
+    uint16_t segment_entry_count;
+    uint16_t section_entry_size;
+    uint16_t section_entry_count;
+    uint16_t string_table_section_index;
 } ELFHeader;
+
+enum SG_TYPES {
+    SG_NULL,
+    SG_LOAD,
+    SG_DYNAMIC,
+    SG_INTERPRETER,
+    SG_NOTE,
+    __SG_SHLIB,  // unused
+    SG_PROGRAM_HEADER
+};
+
+#define SG_EXEC 0x1
+#define SG_WRITE 0x2
+#define SG_READ 0x4
+
+typedef struct {
+    uint32_t type;
+    uint32_t flags;
+    // TODO: what is the diff between adr and off
+    uint64_t offset;
+    uint64_t address;
+    uint64_t __physical_address;  // unused
+    uint64_t file_size;
+    uint64_t memory_size;
+    uint64_t alignment;
+} Segment;
+
+#define SC_WRITE 0x1
+#define SC_ALLOC 0x2
+#define SC_EXEC 0x4
+
+#define SC_UNDEF 0
+
+enum SC_TYPES {
+    SC_NULL,
+    SC_PROGRAM_INFO,
+    SC_SYMBOL_TABLE,
+    SC_STRING_TABLE,
+    SC_RELA,
+    SC_HASH,
+    SC_DYNAMIC,
+    SC_NOTE,
+    SC_UNINIT_SPACE,
+    SC_REL,
+    __SC_SHLIB,  // unused
+    SC_DYNSYM
+};
+
+typedef struct {
+    uint32_t name_offset;
+    uint32_t type;
+    uint64_t flags;
+    // TODO: what is the diff between adr and off
+    uint64_t address;
+    uint64_t offset;
+    uint64_t size;
+    uint32_t link;
+    uint32_t info;
+    uint64_t alignment;
+    uint64_t entry_size;
+} Section;
 
 typedef struct {
     int argc;
     char **argv;
     char **envp;
 } MainArgs;
+
+typedef int main_t(int argc, char **argv, char **envp);
 
 word syscall(word call_num, word a1, word a2, word a3) {
     word retval;
@@ -71,6 +149,7 @@ word syscall(word call_num, word a1, word a2, word a3) {
 #define write(fd, buf, len) syscall(SYS_WRITE, fd, buf, len)
 #define open(path, flags, mode) syscall(SYS_OPEN, path, flags, mode)
 #define close(fd) syscall(SYS_CLOSE, fd, 0, 0)
+#define lseek(fd, offset, whence) syscall(SYS_LSEEK, fd, offset, whence)
 #define exit(exit_code) syscall(SYS_EXIT, exit_code, 0, 0)
 
 size_t strlen(const char *msg) {
@@ -158,14 +237,14 @@ void assert_supported_elf(ELFHeader *header) {
     assert(header->type == ELF_EXEC_DYNAMIC, "Error parsing ELF header: ELF is not of type DYN.");
     assert(header->arch == ELF_AMD64, "Error parsing ELF header: CPU must be AMD64(x86_64).");
     assert(header->version == ELF_VERSION, "Error parsing ELF header: ELF version mismatch.");
+    assert(header->segment_entry_size == sizeof(Segment), "Error parsing ELF header: segment size mismatch!");
+    assert(header->section_entry_size == sizeof(Section), "Error parsing ELF header: section size mismatch!");
 }
 
 void read_elf(int fd) {
     ELFHeader elf;
     read(fd, &elf, sizeof(elf));
     assert_supported_elf(&elf);
-
-    // TODO:
 }
 
 void entry() {
